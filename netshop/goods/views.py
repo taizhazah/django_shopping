c0f625a9268de5ce5a50fea9f160efcb5a92a4ba
Administrator <1205692955@qq.com>
2021-08-05 18:03:23 +0800

    动态显示首页第一次提交

--- netshop/goods/views.py
+++ netshop/goods/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
+from goods.models import *
 
 
 class IndexView(View):
-    def get(self, request):
+    def get(self, request,cid=2):
+        # cid为2代表女装开始
+        cid = int(cid)
+        # 查询所有类别信息
+        categorys = Category.objects.all().order_by('id')
+        # 查询当前类别下的全部信息
+        goodsList = Goods.objects.filter(category_id=cid).order_by('id')
 
-        return render(request,'index.html')
+        return render(request,'index.html',{'category': categorys, 'goodsList': goodsList, 'currentcid':cid})
